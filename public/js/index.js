ee6640614379bd2c5420c7051a085adb3e59ba98
betobd <albertobecerrad@gmail.com>
2021-08-01 19:45:41 -0500

    Vista y descarga de documentos por Categoria

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,32 @@
-const employee_name =document.getElementById('empl_name');
+const employee_name     =document.getElementById('empl_name');
 const employee_lastname =document.getElementById('empl_lastname');
-const employee_city =document.getElementById('empl_city');
-const employee_address =document.getElementById('empl_address');
-const employee_emailP =document.getElementById('empl_email_p');
-const employee_emailE =document.getElementById('empl_email_e');
-const employee_size =document.getElementById('empl_size');
-const employee_id =document.getElementById('empl_id');
-const employee_post =document.getElementById('empl_post');
-const employee_vegan =document.getElementById('empl_veg');
-const employee_phone =document.getElementById('empl_phone');
+const employee_city     =document.getElementById('empl_city');
+const employee_address  =document.getElementById('empl_address');
+const employee_emailP   =document.getElementById('empl_email_p');
+const employee_emailE   =document.getElementById('empl_email_e');
+const employee_size     =document.getElementById('empl_size');
+const employee_id       =document.getElementById('empl_id');
+const employee_post     =document.getElementById('empl_post');
+const employee_vegan    =document.getElementById('empl_veg');
+const employee_phone    =document.getElementById('empl_phone');
 
 
-const btn_search =document.getElementById('btn_consulta');
+const btn_search        =document.getElementById('btn_consulta');
 const select_upload_file= document.getElementById('select_file_button');
 
 const category_docs_section= document.getElementById("category_list_section");
 const upload_file_section = document.getElementById("upload_section");
-const doc_descrp= document.getElementById("text_description");
-const btn_Add_Doc= document.getElementById("btnAddDoc");
+const doc_descrp        = document.getElementById("text_description");
+const btn_Add_Doc       = document.getElementById("btnAddDoc");
+
 
 var user_id ="";
+var categ_doc="";
+var user_name="";
+
+var texto = "texto con espacios/yslach"
+    console.log(texto.replace(/\s+/g, '').replace('/','-'))
+    console.log((new Date).toLocaleString().replace(/\s+/g, '').replace(/[/]/g,'-'));
 
 upload_file_section.style.display = "none";
 select_upload_file.style.display="none";
@@ -27,13 +34,7 @@ select_upload_file.style.display="none";
 btn_search.addEventListener('click', async(e) =>{
       
     const employees = await getUser();
-    const docs = await getDoc();
-
-    docs.forEach(arc =>{
-        arcdata=arc.data();
-        console.log('El link es:'+arcdata.doc_lik);
-    });
-      
+         
    
     let info = {
         name:"",
@@ -55,6 +56,7 @@ btn_search.addEventListener('click', async(e) =>{
         data=doc.data();
         console.log(data.nombre);        
         user_id=doc.id;
+        user_name=data.nombre;
         console.log('El ID es:'+ user_id);
         console.log(typeof(data.nombre));
         info.name=data.nombre;
@@ -91,15 +93,27 @@ btn_search.addEventListener('click', async(e) =>{
 
 select_upload_file.addEventListener('change', async(e) =>{
     const file = e.target.files[0]
-    await set(file,user_id);
+    console.log("el FILE ES:" + file)
+    await set(file,user_id,data.nombre);
+    //upload_file_section.style.display = "none";
 
 } );
 
 
-function myFunction() {
-              
+function show_category_list() {
+    
+    //console.log(value)
+    //category_docs_section.style.display = "block"; 
+    category_docs_section.style.display = "flex";   
+    btn_Add_Doc.style.display="block";
+    upload_file_section.style.display = "none";
+
 }
 
+function myFunction(){
+    console.log(categ_doc);
+    console.log(doc_descrp.value);
+}
 
 function myFunction2() {
           
@@ -107,11 +121,91 @@ function myFunction2() {
     
 }
 
-
-function btn_add(){
+function hide_category_list(){
 
     category_docs_section.style.display = "none";    
     btn_Add_Doc.style.display="none";
     upload_file_section.style.display = "block";
 }
 
+async function genera_tabla(categoryFilter){
+
+  var body = document.getElementsByClassName("show_documents")[0];
+    // Crea un elemento <table> y un elemento <tbody>
+  var tabla   = document.getElementById("docs_table");
+  var tblBody = document.createElement("tbody");
+
+  const docs = await getDoc(categoryFilter);
+
+    docs.forEach(arc =>{
+        arcdata=arc.data();
+        //console.log('El link es:'+arcdata.doc_lik);
+        var fila = document.createElement("tr");
+
+        for (var j = 0; j < 4; j++) {
+            // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+            // texto sea el contenido de <td>, ubica el elemento <td> al final
+            // de la fila de la tabla
+            var celda = document.createElement("td");
+            //var textoCelda = document.createTextNode(arcdata[Object.keys(arcdata)[j]]);
+
+            switch (j) {
+              case 0:
+                var textoCelda = document.createTextNode(arcdata.employee_id);
+                break;
+              case 1:
+                var textoCelda = document.createTextNode(arcdata.descripcion);
+                break;
+
+              case 2:
+                var textoCelda = document.createTextNode(arcdata.categoria);
+                break;
+              case 3:
+                var textoCelda = document.createTextNode(arcdata.doc_lik);
+                console.log(typeof(textoCelda.data));
+                var textoDescargar = document.createTextNode("Descargar");
+                var link = document.createElement("a");
+                link.href=textoCelda.data;
+                link.setAttribute('download',textoCelda.data);
+                link.setAttribute('target','_blank');
+                link.text=textoDescargar.data
+                break;
+              
+            }
+
+            if(j==3){
+              //celda.appendChild(textoDescargar);
+              celda.appendChild(link);
+              fila.appendChild(celda);
+            }else{
+              celda.appendChild(textoCelda);
+              fila.appendChild(celda);
+            }
+            
+
+          }
+
+           
+          // agrega la fila al final de la tabla (al final del elemento tblbody)
+          tblBody.appendChild(fila);
+
+          
+          //console.log("DATA:"+arcdata[Object.keys(arcdata)[1]])
+    });
+
+  
+  // posiciona el <tbody> debajo del elemento <table>
+  tabla.appendChild(tblBody);
+  // appends <table> into <body>
+  body.appendChild(tabla);
+  // modifica el atributo "border" de la tabla y lo fija a "2";
+  tabla.setAttribute("border", "2");
+
+
+}
+
+function selectedCategory(){
+    const selected_category = document.getElementById('doc_selected');
+    categ_doc = selected_category.options[selected_category.selectedIndex].text;
+    console.log(categ_doc);
+}
--- public/js/store.js
+++ public/js/store.js
@@ -1,29 +1,41 @@
+//var moment = require('../../node_modules/moment');  
+
 const db= firebase.firestore();
 const cedula = document.getElementById('cedula');
+//const selected_category = document.getElementById('doc_selected');
 const dbUsers = 'trabajadores';
 const dbDocs = 'documentos';
 const filterDocUser = 'id1';
-const filterDocCategory= 'ARL';
+//const filterDocCategory= 'ARL';
+//const selected_category = document.getElementById('doc_selected');
 
 const  getUser = () => db.collection(dbUsers).where('numero_doc','==',cedula.value).get();
 
-const getDoc = ()=> db.collection(dbDocs).where('employee_id','==','id1').get();
+const getDoc = (categoryFilter)=> db.collection(dbDocs).where('employee_id','==',user_id).where('categoria','==',categoryFilter).get();
 
 
 
-async function set(file,user_id){
+async function set(file,user_id,doc_name){
     
+    //var categ_doc = selected_category.options[selected_category.selectedIndex].text;
     const storageRef = firebase.storage().ref()
-    const fileRef = storageRef.child('Aqui el nombre del archivo O file.name')
+    const filename = doc_name + '_' + categ_doc + '_' +  (new Date).toLocaleString()+'.pdf';
+    const fileRef = storageRef.child(filename.replace(/\s+/g, '').replace(/[/]/g,'-'));
+    
+   
+    
     await fileRef.put(file)
     const fileURL = await  fileRef.getDownloadURL()
 
-    var Docs = db.collection('documentos').doc(user_id);
+    var Docs = db.collection('documentos').doc();
 
     var setWithMerge = Docs.set({
     doc_lik: fileURL,
-    categoria: 'EPS',
-    descripcion: 'Documento de EPS'
+    categoria: categ_doc,
+    descripcion: doc_descrp.value,
+    employee_id: user_id,
+    nombre_doc: filename,
+
 }, { merge: true });
 
     console.log(fileURL)
